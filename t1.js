f82fb89d0544bd38bc533d6fc9cb22911a58c190
Dan Ustilovsky <diustilovsky@uc.cl>
2021-04-03 23:54:40 -0400

    working code

--- t1.js
+++ t1.js
@@ -1,4 +1,5 @@
 const readline = require("readline-sync");
+const _ = require("lodash");
 
 
 const applySpecialPlay = (name) => {
@@ -8,16 +9,25 @@ const applySpecialPlay = (name) => {
     };
   };
 };
-// Una función que reciba el nombre del jugador, su puntaje y sus lanzamientos y devuelva el nuevo score del jugador
-const ingresar_jugada = (score, shots) => {
-  const applyDB = applySpecialPlay('DB')(50);
-  const applySB = applySpecialPlay('SB')(25);
-  newShots = shots.map((shot) => applySB(applyDB(shot)))
-  console.log(newShots)
-  const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue[0]) * parseInt(currentValue[1])
-  console.log(Math.abs(score + 1 - newShots.reduce(reducer, newShots[0])))
-  return Math.abs(score + 1 - newShots.reduce(reducer, newShots[0]))
 
+const applyDB = applySpecialPlay("DB")(50);
+const applySB = applySpecialPlay("SB")(25);
+
+const printWinner = (playerName) => {
+  console.log(`${playerName} ha ganado, Felicitaciones!`);
+};
+
+// Una función que reciba el nombre del jugador, su puntaje y sus lanzamientos y devuelva el nuevo score del jugador
+const ingresar_jugada = (name, score, shots) => {  
+  const turnScore = _
+    .chain(shots)
+    .map((shot) => applySB(applyDB(shot)))
+    .map((shot) => shot[0] * shot[1])
+    .reduce((accumulator, currentValue) => accumulator + currentValue)
+    .value();
+  const updatedScore = Math.abs(score - turnScore);
+  console.log(`${name} queda con ${updatedScore} puntos.`);
+  return updatedScore
 };
 
 // Una función que inicializa el juego dejando a cada jugador con 501 puntos
@@ -29,14 +39,17 @@ const gameLogic = (playersPoints) => {
   [actual, ...rest] = playersPoints;
   [playerName, playerScore] = actual;
   const newPlay = JSON.parse(readline.question(`${playerName} ingrese su jugada >`));
-  const playersStatus = [...rest, [playerName, ingresar_jugada(playerScore, newPlay)]]
+  const playersStatus = [
+    ...rest,
+    [playerName, ingresar_jugada(playerName, playerScore, newPlay)],
+  ];
   playersStatus.some((x) => x[1] === 0) ? printWinner(playerName) : gameLogic(playersStatus)
 };
 
 // Diálogo con el simulador
 const play_game = (players) => {
   const playersPoints = init_game(players);
-  console.log(playersPoints);
+  console.log(`Juego inicializado con los jugadores ${players.join(", ")}.`);
   gameLogic((playersPoints))
 };
 
@@ -45,3 +58,4 @@ const playersNames = readline
   .split(",");
 
 play_game(playersNames);
+
