28bf68ae2dca7905a6bf6d41d37e58641acec59e
Nicolas Rosenberg <nrrosenberg@gmail.com>
2021-04-03 21:59:23 -0300

    ingresar jugada

--- t1.js
+++ t1.js
@@ -9,7 +9,16 @@ const applySpecialPlay = (name) => {
   };
 };
 // Una función que reciba el nombre del jugador, su puntaje y sus lanzamientos y devuelva el nuevo score del jugador
-const ingresar_jugada = (score, shots) => {};
+const ingresar_jugada = (score, shots) => {
+  const applyDB = applySpecialPlay('DB')(50);
+  const applySB = applySpecialPlay('SB')(25);
+  newShots = shots.map((shot) => applySB(applyDB(shot)))
+  console.log(newShots)
+  const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue[0]) * parseInt(currentValue[1])
+  console.log(Math.abs(score + 1 - newShots.reduce(reducer, newShots[0])))
+  return Math.abs(score + 1 - newShots.reduce(reducer, newShots[0]))
+
+};
 
 // Una función que inicializa el juego dejando a cada jugador con 501 puntos
 const init_game = (players) => {
@@ -21,7 +30,7 @@ const gameLogic = (playersPoints) => {
   [playerName, playerScore] = actual;
   const newPlay = JSON.parse(readline.question(`${playerName} ingrese su jugada >`));
   const playersStatus = [...rest, [playerName, ingresar_jugada(playerScore, newPlay)]]
-  (playersStatus.some((x) => x[1] === 0)) ? printWinner(playerName) : gameLogic(playersStatus) 
+  playersStatus.some((x) => x[1] === 0) ? printWinner(playerName) : gameLogic(playersStatus)
 };
 
 // Diálogo con el simulador
